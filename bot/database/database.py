70a184d4aba4ef91929c436281faaa3a7b1dcef3
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-18 14:56:18 +0530

    Update database.py

--- bot/database/database.py
+++ bot/database/database.py
@@ -1,22 +1,14 @@
-import re
+import os
 import motor.motor_asyncio # pylint: disable=import-error
-from bot import DB_URI # pylint: disable=import-error
+from bot import DB_URI
 
-class Singleton(type):
-    __instances__ = {}
+DB_NAME = os.environ.get("DB_NAME", "Adv_Auto_Filter")
 
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls.__instances__:
-            cls.__instances__[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-
-        return cls.__instances__[cls]
-
-
-class Database(metaclass=Singleton):
+class Database:
 
     def __init__(self):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
-        self.db = self._client["Adv_Auto_Filter"]
+        self.db = self._client[DB_NAME]
         self.col = self.db["Main"]
         self.acol = self.db["Active_Chats"]
         self.fcol = self.db["Filter_Collection"]
@@ -166,9 +158,7 @@ class Database(metaclass=Singleton):
                                 channel_id
                             }
                         }
-                    },
-                False,
-                True
+                    }
             )
 
             await self.del_active(group_id, channel_id)
@@ -296,7 +286,6 @@ class Database(metaclass=Singleton):
         return True
 
 
-
     async def del_active(self, group_id: int, channel_id: int):
         """
         A funtion to delete a channel from active chat colletion in db
@@ -304,7 +293,7 @@ class Database(metaclass=Singleton):
         templ = {"$pull": {"chats": dict(chat_id = channel_id)}}
         
         try:
-            await self.acol.update_one({"_id": group_id}, templ, False, True)
+            await self.acol.update_one({"_id": group_id}, templ)
         except Exception as e:
             print(e)
             pass
@@ -344,7 +333,6 @@ class Database(metaclass=Singleton):
         connection = await self.acol.find_one({"_id": group_id})
 
         if connection:
-            self.acache[str(group_id)] = connection
             return connection
         return False
 
@@ -433,7 +421,10 @@ class Database(metaclass=Singleton):
         A Funtion to fetch all similar results for a keyowrd
         from using text index
         """
+        await self.create_index()
 
+        chat = await self.find_chat(group_id)
+        chat_accuracy = float(chat["configs"].get("accuracy", 0.80))
         achats = await self.find_active(group_id)
         
         achat_ids=[]
@@ -444,14 +435,22 @@ class Database(metaclass=Singleton):
             achat_ids.append(chats.get("chat_id"))
         
         filters = []
-        
-        pattern = keyword.lower().strip().replace(' ','.*')
-        raw_pattern = r"\b{}\b".format(pattern)
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
                 
-        db_list = self.fcol.find({"group_id": group_id,"file_name": regex})
+        pipeline= {
+            'group_id': int(group_id), '$text':{'$search': keyword}
+        }
+        
+        
+        db_list = self.fcol.find(
+            pipeline, 
+            {'score': {'$meta':'textScore'}} # Makes A New Filed With Match Score In Each Document
+        )
+
+        db_list.sort([("score", {'$meta': 'textScore'})]) # Sort all document on the basics of the score field
         
         for document in await db_list.to_list(length=600):
+            if document["score"] < chat_accuracy:
+                continue
             
             if document["chat_id"] in achat_ids:
                 filters.append(document)
@@ -476,7 +475,7 @@ class Database(metaclass=Singleton):
             file_id = file.get("file_id")
             file_name = file.get("file_name")
             file_type = file.get("file_type")
-            file_caption = file.get("caption")
+            file_caption = file.get("file_caption")
         return file_id, file_name, file_caption, file_type
 
 
@@ -494,4 +493,4 @@ class Database(metaclass=Singleton):
         """
         return await self.fcol.count_documents({"group_id": group_id})
 
-    
+
